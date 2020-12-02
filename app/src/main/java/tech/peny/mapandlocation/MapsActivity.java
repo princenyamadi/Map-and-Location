d7398dcc7c1d738175bea9f92ae18ae639f4606e
princenyamadi <princenyamadi.pn@gmail.com>
2020-12-02 19:51:49 +0000

    getting geodata

--- app/src/main/java/tech/peny/mapandlocation/MapsActivity.java
+++ app/src/main/java/tech/peny/mapandlocation/MapsActivity.java
@@ -8,11 +8,14 @@ import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -21,6 +24,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -75,6 +82,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                  userLocation = new LatLng(location.getLatitude(), location.getLongitude());
                 mMap.addMarker(new MarkerOptions().position(userLocation).title("Marker"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+
+                    List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(), 1);
+                    if(listAddresses != null && listAddresses.size() > 0){
+                        Log.i("PlaceInfo",listAddresses.get(0).toString());
+
+                        String address  = "";
+
+                        if(listAddresses.get(0).getAdminArea() != null ){
+                            address += listAddresses.get(0).getAdminArea() + " ";
+                        }
+                        if(listAddresses.get(0).getLocality() != null ){
+                            address += listAddresses.get(0).getLocality() + " ";
+                        }
+                        if(listAddresses.get(0).getThoroughfare() != null ){
+                            address += listAddresses.get(0).getThoroughfare() + " ";
+                        }
+                        if(listAddresses.get(0).getPostalCode() != null ){
+                            address += listAddresses.get(0).getPostalCode() + " ";
+                        }
+
+                        Toast.makeText(MapsActivity.this, address, Toast.LENGTH_SHORT).show();
+                        Log.i("Address",address);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 
             }
         };
